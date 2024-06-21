5922d3486c238b60dead67f58b0105f4b0450ac6
Dipto Karmaker <96812332+kdipto1@users.noreply.github.com>
2024-06-21 23:41:38 +0600

    UPDATED: packages updated to latest

--- index.js
+++ index.js
@@ -1,124 +1,144 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongodb_1 = require("mongodb");
 const port = process.env.PORT || 5000;
-const express = require("express");
-const cors = require("cors");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 require("dotenv").config();
-const app = express();
-const jwt = require("jsonwebtoken");
+const app = (0, express_1.default)();
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 //middleware
-app.use(cors());
-app.use(express.json());
-
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
 //Verify token function:
 function verifyJWT(req, res, next) {
-  const authHeader = req.headers.authorization;
-  console.log(authHeader);
-  if (!authHeader) {
-    return res.status(401).send({ message: "Unauthorized access" });
-  }
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-    if (err) {
-      return res.status(403).send({ message: "Forbidden Access", status: 403 });
-    }
-    req.decoded = decoded;
-    next();
+    const authHeader = req.headers.authorization;
     console.log(authHeader);
-  });
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized access" });
+    }
+    const token = authHeader.split(" ")[1];
+    jsonwebtoken_1.default.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res
+                .status(403)
+                .send({ message: "Forbidden Access", status: 403 });
+        }
+        req.decoded = decoded;
+        next();
+        console.log(authHeader);
+    });
 }
-
-//mongodb connect
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p85dy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+//mongodb connect old config --------
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p85dy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   serverApi: ServerApiVersion.v1,
+// });
+// mongodb connect new config ++++++++
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new mongodb_1.MongoClient(process.env.MONGODB_URI, {
+    serverApi: {
+        version: mongodb_1.ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 });
-
-async function run() {
-  try {
-    await client.connect();
-    const inventoryCollection = client.db("stockWorld").collection("items");
-    //post item in database
-    app.post("/inventory",verifyJWT, async (req, res) => {
-      const newItem = req.body;
-      const result = await inventoryCollection.insertOne(newItem);
-      res.send(result);
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield client.connect();
+            const inventoryCollection = client.db("stockWorld").collection("items");
+            //post item in database
+            app.post("/inventory", verifyJWT, (req, res) => __awaiter(this, void 0, void 0, function* () {
+                const newItem = req.body;
+                const result = yield inventoryCollection.insertOne(newItem);
+                res.send(result);
+            }));
+            // Home Page items get api
+            app.get("/homeInventory", (req, res) => __awaiter(this, void 0, void 0, function* () {
+                // const size = parseInt(req.query.size);
+                const query = {};
+                const cursor = inventoryCollection.find(query, { limit: 6 });
+                // items = await cursor.limit(size).toArray();
+                let items = yield cursor.toArray();
+                res.send(items);
+            }));
+            //Get all items from database
+            app.get("/manageInventory", verifyJWT, (req, res) => __awaiter(this, void 0, void 0, function* () {
+                // const size = parseInt(req.query.size);
+                const query = {};
+                const cursor = inventoryCollection.find(query);
+                // let items;
+                // if (size) {
+                // items = await cursor.limit(size).toArray();
+                // } else {
+                const allItems = yield cursor.toArray();
+                // }
+                res.send(allItems);
+            }));
+            //Update single item in database
+            app.put("/inventory/:id", verifyJWT, (req, res) => __awaiter(this, void 0, void 0, function* () {
+                const id = req.params.id;
+                const data = req.body;
+                // console.log(data);
+                const filter = { _id: new mongodb_1.ObjectId(id) };
+                const options = { upsert: true };
+                const updateDoc = { $set: { quantity: data.quantity } };
+                const result = yield inventoryCollection.updateOne(filter, updateDoc, options);
+                res.send(result);
+            }));
+            //Get single item from database
+            app.get("/inventory/:id", verifyJWT, (req, res) => __awaiter(this, void 0, void 0, function* () {
+                const id = req.params.id;
+                const query = { _id: new mongodb_1.ObjectId(id) };
+                const item = yield inventoryCollection.findOne(query);
+                res.send(item);
+            }));
+            //Delete single item from database
+            app.delete("/inventory/:id", verifyJWT, (req, res) => __awaiter(this, void 0, void 0, function* () {
+                const id = req.params.id;
+                const query = { _id: new mongodb_1.ObjectId(id) };
+                const result = yield inventoryCollection.deleteOne(query);
+                res.send(result);
+            }));
+            // Get user products api
+            app.get("/inventoryUser", verifyJWT, (req, res) => __awaiter(this, void 0, void 0, function* () {
+                const email = req.query.email;
+                const query = { email: email };
+                const cursor = inventoryCollection.find(query);
+                const items = yield cursor.toArray();
+                res.send(items);
+            }));
+            //Get token api
+            app.post("/login", (req, res) => __awaiter(this, void 0, void 0, function* () {
+                const email = req.body;
+                console.log(email);
+                const token = yield jsonwebtoken_1.default.sign(email, process.env.ACCESS_TOKEN_SECRET);
+                res.send({ token: token });
+            }));
+        }
+        finally {
+        }
     });
-    // Home Page items get api
-    app.get("/homeInventory", async (req, res) => {
-      // const size = parseInt(req.query.size);
-      const query = {};
-      const cursor = inventoryCollection.find(query, { limit: 6 });
-      // items = await cursor.limit(size).toArray();
-      let items = await cursor.toArray();
-      res.send(items);
-    });
-    //Get all items from database
-    app.get("/manageInventory",verifyJWT, async (req, res) => {
-      // const size = parseInt(req.query.size);
-      const query = {};
-      const cursor = inventoryCollection.find(query);
-      // let items;
-      // if (size) {
-      // items = await cursor.limit(size).toArray();
-      // } else {
-      const allItems = await cursor.toArray();
-      // }
-      res.send(allItems);
-    });
-    //Update single item in database
-    app.put("/inventory/:id",verifyJWT, async (req, res) => {
-      const id = req.params.id;
-      const data = req.body;
-      // console.log(data);
-      const filter = { _id: ObjectId(id) };
-      const options = { upsert: true };
-      const updateDoc = { $set: { quantity: data.quantity } };
-      const result = await inventoryCollection.updateOne(
-        filter,
-        updateDoc,
-        options
-      );
-      res.send(result);
-    });
-    //Get single item from database
-    app.get("/inventory/:id",verifyJWT, async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const item = await inventoryCollection.findOne(query);
-      res.send(item);
-    });
-    //Delete single item from database
-    app.delete("/inventory/:id",verifyJWT, async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await inventoryCollection.deleteOne(query);
-      res.send(result);
-    });
-    // Get user products api
-    app.get("/inventoryUser", verifyJWT, async (req, res) => {
-      const email = req.query.email;
-      const query = { email: email };
-      const cursor = inventoryCollection.find(query);
-      const items = await cursor.toArray();
-      res.send(items);
-    });
-    //Get token api
-    app.post("/login", async (req, res) => {
-      const email = req.body;
-      console.log(email);
-      const token = await jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
-      res.send({ token: token });
-    });
-  } finally {
-  }
 }
 run().catch(console.dir);
-
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 app.listen(port, () => {
-  console.log(`Listening from port ${port}`);
+    console.log(`Listening from port ${port}`);
 });
---
+++ index.ts
@@ -0,0 +1,144 @@
+import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
+const port = process.env.PORT || 5000;
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+require("dotenv").config();
+const app = express();
+import jwt from "jsonwebtoken";
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//Verify token function:
+function verifyJWT(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+  console.log(authHeader);
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_SECRET as string,
+    function (err, decoded) {
+      if (err) {
+        return res
+          .status(403)
+          .send({ message: "Forbidden Access", status: 403 });
+      }
+      (req as any).decoded = decoded;
+      next();
+      console.log(authHeader);
+    }
+  );
+}
+
+//mongodb connect old config --------
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p85dy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   serverApi: ServerApiVersion.v1,
+// });
+
+// mongodb connect new config ++++++++
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI as string, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const inventoryCollection = client.db("stockWorld").collection("items");
+    //post item in database
+    app.post("/inventory", verifyJWT, async (req, res) => {
+      const newItem = req.body;
+      const result = await inventoryCollection.insertOne(newItem);
+      res.send(result);
+    });
+    // Home Page items get api
+    app.get("/homeInventory", async (req, res) => {
+      // const size = parseInt(req.query.size);
+      const query = {};
+      const cursor = inventoryCollection.find(query, { limit: 6 });
+      // items = await cursor.limit(size).toArray();
+      let items = await cursor.toArray();
+      res.send(items);
+    });
+    //Get all items from database
+    app.get("/manageInventory", verifyJWT, async (req, res) => {
+      // const size = parseInt(req.query.size);
+      const query = {};
+      const cursor = inventoryCollection.find(query);
+      // let items;
+      // if (size) {
+      // items = await cursor.limit(size).toArray();
+      // } else {
+      const allItems = await cursor.toArray();
+      // }
+      res.send(allItems);
+    });
+    //Update single item in database
+    app.put("/inventory/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      // console.log(data);
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = { $set: { quantity: data.quantity } };
+      const result = await inventoryCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+    //Get single item from database
+    app.get("/inventory/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const item = await inventoryCollection.findOne(query);
+      res.send(item);
+    });
+    //Delete single item from database
+    app.delete("/inventory/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await inventoryCollection.deleteOne(query);
+      res.send(result);
+    });
+    // Get user products api
+    app.get("/inventoryUser", verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const cursor = inventoryCollection.find(query);
+      const items = await cursor.toArray();
+      res.send(items);
+    });
+    //Get token api
+    app.post("/login", async (req, res) => {
+      const email = req.body;
+      console.log(email);
+      const token = await jwt.sign(
+        email,
+        process.env.ACCESS_TOKEN_SECRET as string
+      );
+      res.send({ token: token });
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`Listening from port ${port}`);
+});
